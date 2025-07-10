6fa58191de5c13fb69d22ed09be1b53a25a98360
Mathu Thavarajah <mthavarajah10@gmail.com>
2025-07-10 19:04:11 -0400

    new

--- app.py
+++ app.py
@@ -4,48 +4,48 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
 
-cred = credentials.Certificate("private/chemsafe---argan-s-lab-firebase-adminsdk-fbsvc-10f6bd9f8c.json")
-firebase_admin.initialize_app(cred)
+# cred = credentials.Certificate("private/chemsafe---argan-s-lab-firebase-adminsdk-fbsvc-10f6bd9f8c.json")
+# firebase_admin.initialize_app(cred)
 
-app.secret_key = 'secret'
+# app.secret_key = 'secret'
 
-# Route for signup page
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    if request.method == 'POST':
-        email = request.form.get['email']
-        password = request.form.get['password']
+# # Route for signup page
+# @app.route('/signup', methods=['GET', 'POST'])
+# def signup():
+#     if request.method == 'POST':
+#         email = request.form.get['email']
+#         password = request.form.get['password']
         
-        try:
-            # Create a new user in Firebase
-            user = auth.create_user(email=email, password=password)
-            flash('User created successfully!', 'success')
-            return redirect(url_for('login'))  # Redirect to the login page after signup
-        except auth.EmailAlreadyExistsError:
-            flash('Email already exists. Try logging in instead.', 'danger')
-        except Exception as e:
-            flash(f'An error occurred: {e}', 'danger')
+#         try:
+#             # Create a new user in Firebase
+#             user = auth.create_user(email=email, password=password)
+#             flash('User created successfully!', 'success')
+#             return redirect(url_for('login'))  # Redirect to the login page after signup
+#         except auth.EmailAlreadyExistsError:
+#             flash('Email already exists. Try logging in instead.', 'danger')
+#         except Exception as e:
+#             flash(f'An error occurred: {e}', 'danger')
     
-    return render_template('signup.html')
+#     return render_template('signup.html')
 
 
-# Route for login page
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
+# # Route for login page
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if request.method == 'POST':
+#         email = request.form['email']
+#         password = request.form['password']
         
-        try:
-            user = auth.get_user_by_email(email)
-            flash(f'Successfully found user {email}', 'success')
-            return redirect(url_for('welcome')) 
-        except auth.UserNotFoundError:
-            flash('No user found with that email.', 'danger')
-        except Exception as e:
-            flash(f'Error: {e}', 'danger')
+#         try:
+#             user = auth.get_user_by_email(email)
+#             flash(f'Successfully found user {email}', 'success')
+#             return redirect(url_for('welcome')) 
+#         except auth.UserNotFoundError:
+#             flash('No user found with that email.', 'danger')
+#         except Exception as e:
+#             flash(f'Error: {e}', 'danger')
     
-    return render_template('login.html')
+#     return render_template('login.html')
 
 
 # Route for welcome page after successful login
